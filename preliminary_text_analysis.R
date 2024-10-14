6643047c9a83046a876a6397fe1b6f1e3cff9587
tmuthusamy <tejas.muthusamy@yale.edu>
2024-10-14 08:08:50 -0400

    Update preliminary_text_analysis.R

--- preliminary_text_analysis.R
+++ preliminary_text_analysis.R
@@ -1,3 +1,24 @@
-final_dataframe <- read.csv("final_dataframe.csv")
+df1 = readRDS("final_dataframe_1.rds")
 
-#
+#Subsetting comments under two sentences
+library(stringr)
+library(tidyverse)
+count_sentences <- function(words) {
+  sentences <- unlist(str_split(words, "(?<=[.!?])\\s+"))
+  return(length(sentences))
+}
+
+df1$comment_sentence_count <- sapply(df1$Comment, count_sentences)
+df1$text_sentence_count <- sapply(df1$text, count_sentences)
+df1$sentence_number <- case_when(
+  df1$comment_sentence_count >= 2 ~ "Important",
+  df1$comment_sentence_count >= 2 ~ "Important",
+  TRUE ~ "Trivial"
+)
+
+df2 = df1 %>%
+  filter(df1$sentence_number == "Important")
+
+#Analyzing organization names
+organizations <- unique(df1$Organization.Name[df1$Organization.Name != ""])
+organizations
